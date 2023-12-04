0b5bfc7202c0b639ad1b82e9dfbad31f3f0f2224
Bismark Adzai <adzaibismark@gmail.com>
2023-12-04 01:17:50 +0000

    commit

--- src/components/Hompepage.js
+++ src/components/Hompepage.js
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './Homepage.css';
-import Trending from './TrendingMovies';
 import Top from './Top';
-import TopRated from './TopRatedMovies.js';
-import TrendingSeries from './TrendingSeries.js';
-import TopRatedSeries from './TopRatedSeries.js';
 import tmdbApi from '../Api/tmdbApi';
 
 function Homepage() {
-  const [searchQuery, setSearchQuery] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
@@ -26,38 +22,29 @@ function Homepage() {
   }, [searchQuery]);
 
   // Handle search query changes
-  const handleSearchChange = () => {
-    setSearchQuery(!searchQuery);
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
   };
 
   return (
     <div className='homepage'>
-      <Top onSearchChange={handleSearchChange} />
-      {searchQuery ? (
+      <Top handleSearchQuery={handleSearchChange} />
+      {searchQuery && (
         <div className="search-results">
           <h1>Search Results</h1>
-            {searchResults.map((movie) => (
-              <div className='movie-grid' key={movie.id}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
-                  alt={movie.title}
-                />
-                <p>{movie.title}</p>
-              </div>
-
-            ))}
+          {searchResults.map((movie) => (
+            <div className='movie-grid' key={movie.id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <p>{movie.title}</p>
+            </div>
+          ))}
         </div>
-      ) : (
-        <>
-          <Trending />
-          <TopRated />
-          <TrendingSeries />
-          <TopRatedSeries />
-        </>
       )}
     </div>
   );
 }
 
 export default Homepage;
-
