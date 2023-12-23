6f2fe417fb7e7e04a4b5b6e083b497de7d6f3369
Bismark Adzai <adzaibismark@gmail.com>
2023-12-23 23:35:25 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css'
 import Homepage from "./components/Hompepage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div>
-      <Homepage />
-
-
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+      </Routes>
+    </Router>
   )
 }
 export default App;
